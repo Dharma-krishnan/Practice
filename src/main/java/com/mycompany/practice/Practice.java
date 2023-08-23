17ac3b4e129c359a0d4d625725261ef63e6250e8
Dharma-krishnan <dharmab2001@gmail.com>
2023-08-23 22:25:14 +0530

    Finding Largest of 3

---
+++ src/main/java/com/mycompany/practice/Practice.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practice;
+
+/**
+ *
+ * @author dharma
+ */
+public class Practice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a=20;
+        int b=30;
+        int c=25;
+        if (a > b && a > c){
+            System.out.println("a is greater");
+            
+        }
+        else if(b > a && b > c){
+            System.out.println("b is greater");
+            
+        }else
+            System.out.println("C is greater");
+    }
+}
