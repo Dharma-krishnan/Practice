bdca7c1881cbad65d24aa26ca5e653edcbd6d9b8
Dharma-krishnan <dharmab2001@gmail.com>
2023-08-27 20:07:08 +0530

    practicing objects and methods :)

--- src/main/java/Inputs.java
+++ src/main/java/Inputs.java
@@ -2,14 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 import java.util.Scanner;
 /**
  *
  * @author dharma
  */
+//public class test{
+//    
+//}
+
 public class Inputs {
-    Scanner obj =new Scanner(System.in);
-    int rollno= obj.nextInt();
-    System.out.println(rollno);
+    public int add(int a,int b){
+        int c=a+b;
+        return c;
     
+}
+    public static void main(String[] args){
+        Inputs ob = new Inputs();
+    Scanner obj =new Scanner(System.in);
+    
+    int num1= obj.nextInt();
+    int num2=obj.nextInt();
+    System.out.println("sum="+ob.add(num1,num2));
+    
+    }
 }
