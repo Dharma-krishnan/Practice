1f062827f02cf715e176ff67a2ff8fa81eb73385
Dharma-krishnan <dharmab2001@gmail.com>
2023-08-28 20:16:32 +0530

    exceptions practice

--- src/main/java/com/mycompany/practice/exceptions.java
+++ src/main/java/com/mycompany/practice/exceptions.java
@@ -9,18 +9,64 @@ package com.mycompany.practice;
  * @author dharma
  */
 public class exceptions {
-    public static void main(String[] args){
+    static void ValidateInput(int number) throws InvalidInputException {
+        if (number > 100) {
+            throw new InvalidInputException("Exception");
+        }
+        System.out.println("Valid Input");
+    }
+
+    public static void main(String[] args) {
+        int number = 101;
+        try {
+            ValidateInput(number);
+        } catch (InvalidInputException e) {
+            System.out.println(e);
+        }
+        
+        
+        
+        
 //        String a=null;
 //        System.out.println(a.length());
+// this example is for arthimetic exception for zer division error
         try{
             int a=20;
             int b=0;
             int c=a/b;
             System.out.print(c);
         }
-//        catch(Exception e){
-//            System.out.println("divide by zero error");
-//        }
+        catch(Exception e){
+            System.out.println(e);  
+        }
+        System.out.println("working after catch");
+        
+        //numberformat error
+        try{
+            int num=Integer.parseInt("string");
+            
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        // array index out of bounds
+        
+         try{  
+             int a[]= new int[6];
+            a[0]=11;
+            a[5]=12;
+            a[6]=12;
+            for(int i=0;i<a.length;i++){
+                System.out.println(a[i]);
+            }
+         }
+         catch(Exception e){
+             System.out.println(e);
+         }
+         
+        
+        
+        
         
         
     }
