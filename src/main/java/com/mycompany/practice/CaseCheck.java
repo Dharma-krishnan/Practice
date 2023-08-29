6e19dadeabb773384d87a12ba40e3b964e3f33dd
Dharma-krishnan <dharmab2001@gmail.com>
2023-08-29 22:10:05 +0530

    basic solving

---
+++ src/main/java/com/mycompany/practice/CaseCheck.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practice;
+import java.util.Scanner;
+/**
+ *
+ * @author dharma
+ */
+public class CaseCheck {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println(in.nextLine());
+    }
+}
---
+++ src/main/java/com/mycompany/practice/Largest.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practice;
+import java.util.Scanner;
+/**
+ *
+ * @author dharma
+ */
+public class Largest {
+    public static void main(String[] args){
+       Scanner obj = new Scanner(System.in);
+        System.out.println("Enter num1: ");
+        int num1 = obj.nextInt();
+        System.out.println("Enter num2: ");
+        int num2 = obj.nextInt();
+        System.out.println("Enter num3: ");
+        int num3 = obj.nextInt();
+        
+        int max = 0;
+        
+        if (num1 > num2 && num1 > num3) {
+            max = num1;
+        } else if (num2 > num3) {
+            max = num2;
+        } else {
+            max = num3;
+        }
+        
+        System.out.println("Largest of three integers is: " + max);
+    }
+}
