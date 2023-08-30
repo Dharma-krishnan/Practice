06d7968a2d21bd1f48191bd1f062349d0d68b0fc
Dharma-krishnan <dharmab2001@gmail.com>
2023-08-30 11:54:24 +0530

    fibonnaci seriesz

--- src/main/java/com/mycompany/practice/FIbo.java
+++ src/main/java/com/mycompany/practice/FIbo.java
@@ -3,23 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.practice;
-import java.util.Scanner;
+
 /**
  *
  * @author dharma
  */
-public class CaseCheck {
+public class FIbo {
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-   
-        char op ='a';
-        if(op =='A' ){
-            System.out.println("A is in UPperCase");
-        }else{
-        System.out.println("a is inlowercase");
+        int n=7;
+    int  a=0;
+     int b=1;
+     int count=2;
+     while(count < n){
+         System.out.print(a);
+         
+         int temp =b;
+         b=b+a;
+         a=temp;
+           
+        count++;
+      
+     }
+         System.out.println(b);
+     
+     
     }
-        
-        
-        
-    }
+    
 }
