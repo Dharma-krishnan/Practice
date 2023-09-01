cc1872c5be5e82c53f3e778c1038e2cdc7030d90
Dharma-krishnan <dharmab2001@gmail.com>
2023-09-01 21:49:11 +0530

    static method

---
+++ src/main/java/com/mycompany/practice/methods.java
@@ -0,0 +1,23 @@
+package com.mycompany.practice;
+import java.util.Scanner;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dharma
+ */
+public class methods {
+    public static void main(String[] args){
+       sum();
+    }
+    static String sum(){
+        Scanner obj= new Scanner(System.in);
+        System.out.println("hii");
+        String h="hiii";
+        return h;
+    }
+    
+}
