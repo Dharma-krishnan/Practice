3958a70fea03efb9dea130144781e46000e3d7c8
Dharma-krishnan <dharmab2001@gmail.com>
2023-09-07 21:20:31 +0530

    arrays depth

---
+++ src/main/java/Problems/arrays.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Problems;
+
+/**
+ *
+ * @author dharma
+ */
+import java.util.*;
+public class arrays {
+    public static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        int ros[];// declaration of array 
+        ros= new int[5];//initialization
+        System.out.println(ros.length);
+//        for(int i=0;i<5;i++){
+//            System.out.println("Enter element for "+i+":");
+//            ros[i]=obj.nextInt();
+//        }
+//        for(int i=0;i<5;i++){
+//            System.out.print(ros[i]);
+//        }
+        
+    }
+    
+}
